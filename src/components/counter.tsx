540dd78dd3ea67c508f4d1a8ca494122c7e3b551
Yohann <yohannjaffres88@gmail.com>
2024-05-30 10:20:36 +0200

    Add LICENSE Lint fix

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -2,8 +2,7 @@
 
 import React from "react";
 
-import { Button } from "./ui/button";
-
+import { Button } from "@/components/ui/button";
 import { countStore } from "@/stores/count.store";
 
 export default function Counter() {
